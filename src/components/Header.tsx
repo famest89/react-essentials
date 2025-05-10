4add2aac0ac3c249d355f53cffee54c3dc73ae78
fernando <famest89@gmail.com>
2025-05-10 11:53:22 +0700

    starter code

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import logoImg from '../assets/react-core-concepts.png';
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logoImg} alt='Stylized atom' />
+
+      <h1>React Essentials</h1>
+
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build
+      </p>
+    </header>
+  );
+};
+
+export default Header;
